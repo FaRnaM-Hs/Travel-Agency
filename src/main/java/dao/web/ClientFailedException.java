565d48609660f95e715bb6b3a362918b0a17a7b7
Farnam <farnam.hs@yahoo.com>
2022-12-25 14:02:20 +0330

    Web Server client created

---
+++ src/main/java/dao/web/ClientFailedException.java
@@ -0,0 +1,19 @@
+package dao.web;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class ClientFailedException extends RuntimeException {
+
+    public ClientFailedException(URISyntaxException e) {
+        super(e);
+    }
+
+    public ClientFailedException(IOException e) {
+        super(e);
+    }
+
+    public ClientFailedException(InterruptedException e) {
+        super(e);
+    }
+}
